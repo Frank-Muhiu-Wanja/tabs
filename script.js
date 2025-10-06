af97d46ebeb6a79658e4d3a9be694d73dc05d340
Frank_Onyx <frankmuhiu648@gmail.com>
2025-10-07 02:38:03 +0300

    initial commit-tabs files

---
+++ script.js
@@ -0,0 +1,21 @@
+function getTab(el) {
+  // active
+  const active = document.querySelector(".active");
+  // visible
+  const visible = document.querySelector(".content-visible");
+  // tabContent
+  const tabContent = document.getElementById(el.href.split("#")[1]);
+
+  active.classList.toggle("active");
+  visible.classList.toggle("content-visible");
+
+  el.classList.add("active");
+  tabContent.classList.add("content-visible");
+}
+
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.matches(".tab-item a")) {
+    getTab(e.target);
+  }
+});
